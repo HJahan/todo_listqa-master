953b75dd8d809e2e0eed25abcb0bcc1cb929047a
Humera Jahan <Humera.Jahan@sage.com>
2022-09-25 15:52:26 +0100

    .

--- base/views.py
+++ base/views.py
@@ -52,7 +52,7 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ['title', 'description', 'complete']
+    fields = ['title', 'description']
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
@@ -62,7 +62,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = ['title', 'description', 'complete']
+    fields = ['title', 'description']
     success_url = reverse_lazy('tasks')
 
 class DeleteView(LoginRequiredMixin, DeleteView):
